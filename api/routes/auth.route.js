1cc7b69d5567fdb7558c460ab1af1a2546604d18
Ismail-Bashir <ismail.ahmed@mail.utoronto.ca>
2023-10-16 16:00:14 -0400

    create sign in api route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,10 +1,13 @@
 import express from 'express';
 
 import { signup } from '../controllers/auth.contraller.js';
+import { signin } from '../controllers/auth.contraller.js';
 
 
 const router = express.Router();
 
 router.post("/signup", signup);
+router.post("/signin", signin);
+
 
 export default router;
